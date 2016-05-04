001d01fc8109f99433ce55c7a4daacb7cb5dddc9
André Santos <andre.leonhardt.santos@gmail.com>
2016-05-03 21:40:10 -0300

    aula

--- aula0204/app.js
+++ aula0204/app.js
@@ -1,4 +1,95 @@
-var express = require('express');
+var express   =     require("express");
+var bodyParser  =    require("body-parser");
+var app       =     express();
+var controle = false;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/',function(req,res){
+  controle = false;
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/:username',function(req,res){
+  if(controle){
+  res.send('Bem Vindo ' + req.params.username);
+  res.end();
+} else {
+   res.sendStatus(403);
+}
+});
+
+
+var sampleDB = [
+{
+  name: 'admin'
+  , pwd: '123'
+}
+, {
+  name: 'editor'
+  , pwd: '123'
+}
+];
+
+function wellcome(username){
+  return "/" + username;
+};
+
+app.post('/login',function(req,res){
+  var user_name=req.body.user;
+  var password=req.body.password;
+
+  var objVerificar = {
+    name: user_name
+    , pwd: password
+  };
+
+  if(sampleDB.filter(function(objTeste){
+    for(var prop in objTeste){
+
+      console.log('prop = '+ prop);
+      if(objTeste[prop] != objVerificar[prop]){
+        return false;
+      }
+      
+    }
+    return true;
+  }).length == 1){
+    controle = true;
+    res.send(wellcome(user_name));
+
+
+  console.log("From Client pOST request: User name = " + user_name + " and password is " + password);
+  res.end("yes");
+
+    } else {
+      controle = false;
+      res.sendStatus(403, {msg: 'Não Permitido'});
+    
+  }
+
+
+});
+
+
+app.listen(3000,function(){
+  console.log("Started on PORT 3000");
+})
+
+
+
+
+
+/*
+
+
+ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -58,3 +149,4 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+// */
